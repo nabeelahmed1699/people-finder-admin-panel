bc347930a5bfa6f6ca9d111c889811e37383adee
Nabeel Ahmed <nabeelahmed@eduportal.pk>
2023-03-20 12:05:13 +0500

    dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+// @mui
+import Grid from "@mui/material/Grid"
 
+
+// @components
+import CountingCard from 'src/features/dsahboard/components/coutingCard';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={3}>
+            <CountingCard title="Total recovered" total={1352831} color="info" icon={<AcUnitIcon/>} />
+      </Grid>
+      
+      <Grid item xs={12} sm={6} md={3}>
+            <CountingCard title="Recoverd march" total={714000} icon={<AcUnitIcon/>} />
+          </Grid>
+
+
+          <Grid item xs={12} sm={6} md={3}>
+            <CountingCard title="Total reports" total={1723315} color="warning" icon={<AcUnitIcon/>} />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <CountingCard title="Report march" total={234} color="error" icon={<AcUnitIcon/>} />
+          </Grid>
+    </Grid>
   )
 }
 
